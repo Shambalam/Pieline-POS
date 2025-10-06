d5de068aca0b51d5cd7630b48b248faa1c5280b7
Shambalam <dan.cararo@gmail.com>
2025-10-06 16:16:20 -0400

    initial commit

---
+++ LoginPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POSApp
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : UserControl
+    {
+        private MainWindow _mainWindow;
+        public LoginPage(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+        }
+
+        public void onLoginClick(object sender, RoutedEventArgs e)
+        {
+            
+            if(string.IsNullOrEmpty(UsernameBox.Text) || string.IsNullOrEmpty(PasswordBox.Password))
+            {
+                MessageBox.Show("Please enter both username and password.", "Input Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+                       
+            if (UsernameBox.Text.Equals("admin") && PasswordBox.Password.Equals("123"))
+            {
+                _mainWindow.showMenu();
+            }
+        }
+    }
+}
